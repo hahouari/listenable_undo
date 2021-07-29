8a8b0fd370056565496fb00c2ef0b96a800f0e2b
Hocine Abdellatif Houari <h.hocine.abdellatif@gmail.com>
2021-07-29 16:53:23 +0100

    initial commit

---
+++ lib/listenable_undo.dart
@@ -0,0 +1,42 @@
+library listenable_undo;
+
+import 'package:flutter/foundation.dart';
+import 'package:undo/undo.dart';
+
+class ListenableStack extends ChangeStack with ChangeNotifier {
+  @override
+  void add<T>(Change<T> change) {
+    super.add(change);
+    notifyListeners();
+  }
+
+  @override
+  void addGroup<T>(List<Change<T>> changes) {
+    super.addGroup(changes);
+    notifyListeners();
+  }
+
+  @override
+  void undo() {
+    if (canUndo) {
+      super.undo();
+      notifyListeners();
+    }
+  }
+
+  @override
+  void redo() {
+    if (canRedo) {
+      super.redo();
+      notifyListeners();
+    }
+  }
+
+  @override
+  void clearHistory() {
+    if (canUndo || canRedo) {
+      super.clearHistory();
+      notifyListeners();
+    }
+  }
+}
